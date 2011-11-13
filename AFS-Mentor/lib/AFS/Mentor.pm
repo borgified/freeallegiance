3c4138fc6f1b1b6c6ac9e6752c938dc736aef2a9
Fwiffo <borgified@gmail.com>
2011-11-13 01:15:34 -0800

    gotta figure out how to use templates then figure out how to deploy

--- AFS-Mentor/lib/AFS/Mentor.pm
+++ AFS-Mentor/lib/AFS/Mentor.pm
@@ -62,53 +62,10 @@ post '/' => sub {
             print "<tr><td>$freq{$hour}</td></tr>";
         }
         print "</table></td>";
-    
-#        $output->{$i++} = {
-#            callsign => $callsign,
-#            freq     => \%freq,
-#        }
+   
     }
     print "</table>";
-    #template 'output', { output => $output };
-    #print Dumper($output);   
-};
-
-get '/seek_master/:callsign' => sub {
-    my $data=database->prepare('select timestamp,players from currentplayers where timestamp > date_sub(now(),interval 1 month)');
-    $data->execute();
-    #print Dumper($data->fetchrow_hashref);
-    my %result;
-    my $callsign=params->{callsign};
-    my $ok_chars = 'a-zA-Z0-9 ,-';   
-    $callsign =~ s/[^$ok_chars]//go;
-    
-    while(my $a=$data->fetchrow_hashref){ 
-        $result{$$a{timestamp}}=$$a{players};
-    }
-    
-    my %freq;
-    
-    foreach my $timestamp (keys %result) {
-#        if($result{$timestamp} =~ /$callsign\@?\w*\((\d+)\)/i){
-         if($result{$timestamp} =~ /$callsign\@?\w*/i){
-             $timestamp=~/\d (\d\d):/;
-             if(!exists($freq{$1})){
-                 $freq{$1}=1;
-             }else{
-                 $freq{$1}=$freq{$1} + 1;
-             }
-            #print "found $1 $timestamp $result{$timestamp}\n";
-        }
-    }
-    foreach my $hour (sort keys %freq){
-        print "hour: $hour freq: $freq{$hour}\n";
-    }
-    undef(%result);
-    template 'master', {callsign => params->{callsign}, data=>\%result};
-};
-
-get '/seek_apprentice/:callsign' => sub {   
-    template 'apprentice', {callsign => params->{callsign}};
+   
 };
 
 true;
